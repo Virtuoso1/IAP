ac1c34d4918fb4e81d0b159a09e6d9d021089327
Virtuoso1 <waynekimutai20@gmail.com>
2025-11-24 12:29:27 +0300

    r

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SafeSpace</title>
-    @vite(['resources/css/app.css', 'resources/js/app.js'])
+    {{--@vite(['resources/css/app.css', 'resources/js/app.js'])--}}
 </head>
 <body class="bg-gray-50 font-sans">
 
